ece1376666d698f174fe0257915ac6831340975b
AsPulse <aspulse.x@gmail.com>
2021-08-31 02:40:00 +0900

    Add: main HTML File

--- web/index.js
+++ web/index.js
@@ -1,9 +1,12 @@
 'use strict'
 
-const fastify = require('fastify')()
+const fastify = require('fastify')();
+const fs = require('fs').promises;
 
-fastify.get('/', function (req, reply) {
-    reply.send("This is main page");
+fastify.get('/', async (_req, reply) => {
+    const buffer = await fs.readFile('./content/view.html');
+    reply.type('text/html');
+    reply.send(buffer);
 });
 
 fastify.listen(8080);
