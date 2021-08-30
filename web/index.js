3f8c20c3d98276278177574cc434b14a63cc8109
AsPulse <aspulse.x@gmail.com>
2021-08-30 18:52:25 +0000

    Add: save/load algorithm

--- web/index.js
+++ web/index.js
@@ -1,7 +1,8 @@
 'use strict'
 require('dotenv').config();
-const fastify = require('fastify')();
+const fastify = require('fastify')({logger: true});
 const fs = require('fs').promises;
+const fsN = require('fs');
 
 fastify.get('/', async (_req, reply) => {
     const buffer = await fs.readFile('./content/view.html');
@@ -9,4 +10,13 @@ fastify.get('/', async (_req, reply) => {
     reply.send(buffer);
 });
 
+fastify.post('/save', (req, reply) => {
+    reply.send({ result: 'Writing' });
+    fs.writeFile('../content.txt', req.body.data);
+});
+
+fastify.get('/load', async (_req, reply) => {
+    if(!fsN.existsSync('../content.txt')) await fs.writeFile('../content.txt', '');
+    reply.send({ data: (await fs.readFile('../content.txt')).toString() });
+});
 fastify.listen(process.env.PORT ?? 8080);
